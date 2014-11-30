b34d2d921e145f427d0ccf9b1196cf48a7bc4896
Evan Hazlett <ejhazlett@gmail.com>
2014-11-30 15:10:32 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+var (
+	mux = http.NewServeMux()
+)
+
+type (
+	Content struct {
+		Title    string
+		Hostname string
+	}
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("index.html.tmpl")
+	if err != nil {
+		fmt.Println("error loading template: %s", err)
+		return
+	}
+
+	title := os.Getenv("TITLE")
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown"
+	}
+
+	cnt := &Content{
+		Title:    title,
+		Hostname: hostname,
+	}
+
+	t.Execute(w, cnt)
+}
+
+func main() {
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	mux.HandleFunc("/", index)
+
+	fmt.Println("listening on :8080")
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		fmt.Errorf("error serving: %s", err)
+	}
+}
