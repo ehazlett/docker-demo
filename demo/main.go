469bd46d3a2eda19fb3760a6ebfd0fffd298c0a8
Evan Hazlett <ejhazlett@gmail.com>
2014-11-30 22:07:20 -0500

    added pg database example

--- demo/main.go
+++ demo/main.go
@@ -1,32 +1,57 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
 var (
-	mux = http.NewServeMux()
+	mux       = http.NewServeMux()
+	DS        *Datastore
+	DbHost    string
+	DbUser    string
+	DbPass    string
+	DbPort    int
+	DbName    string
+	DbSSLMode string
 )
 
 type (
 	Content struct {
 		Title    string
 		Hostname string
+		Entries  []*LogEntry
 	}
 )
 
+func init() {
+	flag.StringVar(&DbHost, "db-host", "", "database host")
+	flag.IntVar(&DbPort, "db-port", 0, "database port")
+	flag.StringVar(&DbUser, "db-user", "", "database username")
+	flag.StringVar(&DbPass, "db-pass", "", "database password")
+	flag.StringVar(&DbName, "db-name", "", "database name")
+	flag.StringVar(&DbSSLMode, "db-ssl", "", "database ssl mode")
+}
+
+func loadTemplate(filename string) (*template.Template, error) {
+	return template.ParseFiles(filename)
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("index.html.tmpl")
+	t, err := loadTemplate("templates/index.html.tmpl")
 	if err != nil {
-		fmt.Println("error loading template: %s", err)
+		fmt.Printf("error loading template: %s\n", err)
 		return
 	}
 
 	title := os.Getenv("TITLE")
+
 	hostname, err := os.Hostname()
 	if err != nil {
 		hostname = "unknown"
@@ -40,9 +65,90 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, cnt)
 }
 
+func dbIndex(w http.ResponseWriter, r *http.Request) {
+	DS.Add(&LogEntry{
+		Date: time.Now(),
+		Addr: r.RemoteAddr,
+		Path: r.URL.Path,
+	})
+	t, err := loadTemplate("templates/db.html.tmpl")
+	if err != nil {
+		fmt.Printf("error loading template: %s\n", err)
+		return
+	}
+
+	title := os.Getenv("TITLE")
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown"
+	}
+
+	entries, err := DS.GetAll()
+	if err != nil {
+		log.Printf("error retrieving data: %s\n", err)
+	}
+
+	cnt := &Content{
+		Title:    title,
+		Hostname: hostname,
+		Entries:  entries,
+	}
+
+	t.Execute(w, cnt)
+}
+
 func main() {
+	flag.Parse()
+	// load vars from env
+	if DbHost == "" {
+		DbHost = os.Getenv("DB_HOST")
+	}
+
+	if DbPort == 0 {
+		port := os.Getenv("DB_PORT")
+		if port != "" {
+			p, err := strconv.Atoi(port)
+			if err != nil {
+				log.Fatalf("unable to parse database port: %s", err)
+			}
+
+			DbPort = p
+		}
+	}
+
+	if DbUser == "" {
+		DbUser = os.Getenv("DB_USER")
+	}
+
+	if DbPass == "" {
+		DbPass = os.Getenv("DB_PASS")
+	}
+
+	if DbName == "" {
+		DbName = os.Getenv("DB_NAME")
+	}
+
+	sslMode := os.Getenv("DB_SSL_MODE")
+	if sslMode != "" && DbSSLMode == "" {
+		DbSSLMode = sslMode
+	}
+
+	if DbHost == "" || DbPort == 0 || DbUser == "" || DbName == "" {
+		log.Fatalf("you must specify db-host, db-port, db-user and db-name")
+	}
+
+	ds, err := NewDatastore(DbHost, DbPort, DbUser, DbPass, DbName, DbSSLMode)
+	if err != nil {
+		log.Fatalf("unable to connect to datastore: %s", err)
+	}
+
+	DS = ds
+	defer DS.Close()
+
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	mux.HandleFunc("/", index)
+	mux.HandleFunc("/db", dbIndex)
 
 	log.Println("listening on :8080")
 
